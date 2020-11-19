876f2bd06e60724540c843a2553dedded7982ead
zach <zhill091@gmail.com>
2020-11-18 23:01:52 -0500

    Apply colorbar patch

--- config.def.h
+++ config.def.h
@@ -1,22 +1,35 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 30;       /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Hack-regular:size=18" };
 static const char dmenufont[]       = "Hack-regular:size=18";
+static const char col_black[] 	    = "#000000";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
+static const char col_purpl[]       = "#673ab7";
+static const char *colors[][7]      = {
+	/* color bar settings				*/
+	/* SchemeStatus   -> status bar right		*/
+	/* SchemeTagsSel  -> tag bar left selected	*/
+	/* SchemeTagsNorm -> tag bar left unselected	*/
+	/* SchemeInfoSel  -> info bar middle selected	*/
+	/* SchemeInfoNorm -> info bar middle unselected	*/
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] 	 = { col_gray4, col_gray1, col_gray1 },
+	[SchemeSel]  	 = { col_gray4, col_cyan,  col_purpl },
+	[SchemeStatus] 	 = { col_purpl, col_gray1, "#000000" },
+	[SchemeTagsSel]  = { col_gray4, col_purpl, "#000000" },
+	[SchemeTagsNorm] = { col_gray1, col_purpl, "#000000" },
+	[SchemeInfoSel]  = { col_purpl, col_gray1, "#000000" },
+	[SchemeInfoNorm] = { col_gray1, col_gray1, "#000000" },
 };
 
 /* tagging */
